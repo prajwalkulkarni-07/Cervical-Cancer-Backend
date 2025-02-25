d3b681777d9458d858bad9fe53dadb34149258f4
Prajwal Kulkarni <mkprajwal07@gmail.com>
2025-02-25 15:41:41 +0530

    Added port number

--- app.py
+++ app.py
@@ -193,4 +193,5 @@ port = int(os.environ.get("PORT", 5000))
 logger.info(f"Starting app on port {port}")
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
